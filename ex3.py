6515b42c8a4789737c30ea861904f3080967e148
Gustavo Rodrigues <102631537+GuuhRodrigues@users.noreply.github.com>
2023-02-08 13:41:58 -0300

    Add files via upload
    Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.

---
+++ ex3.py
@@ -0,0 +1,9 @@
+# Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+import random
+
+vetor = []
+
+for i in range(10):
+    vetor = float(random.randrange(0,100))
+    i+=1
+    print(vetor)
