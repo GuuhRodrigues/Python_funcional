f3f5db22e8f9b5cd61598e91fd75de5541826bfd
Gustavo Rodrigues <102631537+GuuhRodrigues@users.noreply.github.com>
2023-07-04 20:59:14 -0300

    Add files via upload
    # Faça um programa que leia um ano qualquer e mostre se ele é bissexto.

---
+++ ex33.py
@@ -0,0 +1,14 @@
+# Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
+
+ano = int(input('Entre com o ano: '))
+
+if ano % 4 == 0:
+    print('Este ano é bissexto')
+
+else:
+    print('Este ano não é bissexto')
+
+
+
+
+
