ac5ab454c8404216c8df8dd882d370885338db57
Gustavo Rodrigues <102631537+GuuhRodrigues@users.noreply.github.com>
2023-07-04 20:58:18 -0300

    Add files via upload
    # Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.

---
+++ ex31.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+num = int(input('Entre com um número: '))
+
+if num % 2 == 0:
+    print('O número digitado é par')
+else:
+    print('O número digitado é impar')
